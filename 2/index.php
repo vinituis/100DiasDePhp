8bf40bd97f534e88ce2c98fcdd569d3e7b628732
Vinicius Fernandes <vinicius.turu@gmail.com>
2023-01-02 13:09:51 -0300

    Dia 2 Concluído

---
+++ 2/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$frase = 'Quando eu era bem pequeno eu assisti o filme Procurando Nemo';
+
+$palavra = 'Nemo';
+
+echo 'O nemo está na posição ' . strpos($frase, $palavra) . ' da frase "' . $frase . '"';
+
+?>
